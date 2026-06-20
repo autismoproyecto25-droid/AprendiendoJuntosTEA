45287fbc99fbb53f86dcbd8c7c97592f61fda6a2
autismoproyecto25-droid <autismoproyecto25@gmail.com>
2026-06-20 14:07:36 -0600

    Actualizacion general: nuevas secciones, animaciones y mejoras UI

--- js/main.js
+++ js/main.js
@@ -19,3 +19,61 @@ answer.style.display="block";
 });
 
 });
+
+const topBtn = document.getElementById("topBtn");
+
+window.onscroll = function(){
+
+if(document.documentElement.scrollTop>500){
+
+topBtn.style.display="block";
+
+}
+
+else{
+
+topBtn.style.display="none";
+
+}
+
+};
+
+topBtn.onclick=function(){
+
+window.scrollTo({
+
+top:0,
+
+behavior:"smooth"
+
+});
+
+};
+
+const observer = new IntersectionObserver(entries=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.classList.add("show");
+
+}
+
+});
+
+});
+
+const hiddenElements=document.querySelectorAll(".hidden");
+
+hiddenElements.forEach(el=>observer.observe(el));
+
+const toggle=document.querySelector(".menu-toggle");
+
+const nav=document.querySelector(".nav-links");
+
+toggle.addEventListener("click",()=>{
+
+nav.classList.toggle("active");
+
+});
